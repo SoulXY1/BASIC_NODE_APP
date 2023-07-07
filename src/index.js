886a6593912181234bf37040ca200ba008f7beb9
Eduardo Briseño <88725365+SoulXY1@users.noreply.github.com>
2023-07-06 19:17:46 -0600

    basic_node_app

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express"
+import ejs from "ejs"
+import { dirname, join } from "path"
+import { fileURLToPath } from "url"
+
+import indexRoutes from "./routes/index.routes.js"
+
+const app = express()
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set("views", join(__dirname, "views"))
+
+app.set("view engine", "ejs")
+
+app.use(indexRoutes)
+
+app.use(express.static(join(__dirname, 'public')))
+
+app.listen(3000)
+console.log("Server on port", 3000)
